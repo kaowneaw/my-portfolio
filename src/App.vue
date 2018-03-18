d3b984e3d24cb406a046cc4ab07fefcbf0914f13
panyaBas <th.panya.bas@gmail.com>
2018-03-18 20:31:50 +0700

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app" class="container">
-    <router-view/>
+  <div>
+    <div id="app" class="container">
+      <router-view/>
+    </div>
+    <footer class="footer">
+      Copyright @ by PanyaBas 2018
+    </footer>
   </div>
 </template>
 
@@ -19,5 +24,15 @@ export default {
     font-family: 'Roboto', sans-serif;
     background-color: #F4F7F6 !important;
     -webkit-font-smoothing: antialiased;
+  }
+  .footer {
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #3D4351;
+    height: 52px;
+    font-size: 12px;
+    color: white;
   }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,7 +21,7 @@
             <div class="item-info">
               <img src="../assets/phone.png" width="32px" height="32px" class="float-left">
               <div>
-                092-9986349
+                092-9956349
               </div>
             </div>
             <div class="item-info">
@@ -68,89 +68,156 @@
     <div class="content-portfolio">
       <h1 class="text-center title-head">My Portfolio</h1>
       <div class="row">
-        <div class="col-md-4">
-          <div class="card form-group">
-            <a href="#" target="_blank">
-              <img class="card-img-top" src="../assets/nb-iot.png" alt="Card image cap">
+        <div class="col-md-4" v-for="(portfolio, index) in portfolios" :key="index">
+          <div class="card form-group" @click="showModal(portfolio.gallery)">
+              <img class="card-img-top" :src="portfolio.img" alt="Card image cap">
               <div class="card-body">
                 <div class="card-text">
-                  <h5>Magellan NB-IOT</h5>
-                  <small>IOT Platform</small>
+                  <h5>{{portfolio.title}}</h5>
+                  <small>{{portfolio.desc}}</small>
+                  <a :href="portfolio.src.link" class="float-right" target="_blank">
+                    <img src="../assets/chrome_ico.png" width="25px" height="25px" v-if="portfolio.src.type === 'web'" >
+                    <img src="../assets/android_ico.png" width="25px" height="25px" v-if="portfolio.src.type === 'android'">
+                  </a>
                 </div>
               </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card form-group">
-            <a href="https://www.aischatapp.com" target="_blank">
-              <img class="card-img-top" src="../assets/chat-platform.jpg" alt="Card image cap">
-              <div class="card-body">
-                <div class="card-text">
-                  <h5>Chat Platform</h5>
-                  <small>Chat for Organize</small>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card form-group">
-            <a href="#" target="_blank">
-              <img class="card-img-top" src="../assets/doctor.jpg" alt="Card image cap">
-              <div class="card-body">
-                <div class="card-text">
-                  <h5>AIS Wellness</h5>
-                  <small>Healthcare Platform</small>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card form-group">
-            <a href="https://play.google.com/store/apps/details?id=th.co.mimo.aistcdc&hl=th" target="_blank">
-              <img class="card-img-top" src="../assets/dc.png" alt="Card image cap">
-              <div class="card-body">
-                <div class="card-text">
-                  <h5>AIS D.C.</h5>
-                  <small>Application for AIS D.C.</small>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card form-group">
-            <a href="https://play.google.com/store/apps/details?id=th.co.mimo.aistcdc&hl=th" target="_blank">
-              <img class="card-img-top" src="../assets/ais-fiber.jpg" alt="Card image cap">
-              <div class="card-body">
-                <div class="card-text">
-                  <h5>AIS FIBER ADMIN</h5>
-                  <small>Application Management Fiber Internet</small>
-                </div>
-              </div>
-            </a>
           </div>
         </div>
       </div>
     </div>
+    <div class="content-portfolio">
+      <h1 class="text-center title-head">My Skills</h1>
+      <div class="row">
+        <div class="offset-md-2 col-md-8">
+          <div v-for="(skill, index) in skills" class="row mb-1" :key="index">
+            <div class="col-sm-2">{{ skill.variant }}:</div>
+            <div class="col-sm-10 pt-1">
+              <b-progress :value="skill.value" :variant="skill.variant" :key="skill.variant"></b-progress>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Modal Component -->
+    <b-modal ref="myModalRef" title="รูปภาพ" size="lg" centered :hide-footer="true">
+      <b-carousel :interval="4000" controls indicators background="#ababab" img-height="480" v-model="slide" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
+        <b-carousel-slide v-for="(imgPreview, index) in slidingImg" :img-src="imgPreview" :key="index"></b-carousel-slide>
+      </b-carousel>
+    </b-modal>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      slide: 0,
+      sliding: null,
+      slidingImg: [],
+      portfolios: [
+        {
+          title: 'Magellan NB-IOT',
+          desc: 'IOT Platform',
+          img: 'https://www.img.in.th/images/238c514c7f58ef79d9851b127a6ea90d.jpg',
+          gallery: [
+            'https://www.img.in.th/images/238c514c7f58ef79d9851b127a6ea90d.jpg',
+            'https://www.img.in.th/images/2ddbf13d458a7d14e65a439b7ea76feb.jpg',
+            'https://www.img.in.th/images/93f5e8c8d7ac163f1eba3144e48e8854.jpg'
+          ],
+          src: {
+            type: 'web',
+            link: ''
+          }
+        },
+        {
+          title: 'Chat Platform',
+          desc: 'Chat for Organize',
+          img: 'https://www.picz.in.th/images/2018/03/17/Sxw4qv.jpg',
+          gallery: [
+            'https://www.picz.in.th/images/2018/03/17/Sxw4qv.jpg',
+            'https://www.img.in.th/images/88b7b9c1b03fde95b9862b851eb8e1cc.jpg',
+            'https://www.img.in.th/images/f8c8f6aed04a092530787f46bdff4694.jpg',
+            'https://www.img.in.th/images/19170d5265e15cfd38a719c7d5f6c736.jpg'
+          ],
+          src: {
+            type: 'web',
+            link: 'https://www.aischatapp.com/'
+          }
+        },
+        {
+          title: 'AIS Wellness',
+          desc: 'Healthcare Platform',
+          img: 'https://www.picz.in.th/images/2018/03/17/SxwfSN.jpg',
+          gallery: [
+            'https://www.picz.in.th/images/2018/03/17/SxwfSN.jpg',
+            'https://www.img.in.th/images/0b3b7d576224696b7ccac7b162e33cfb.jpg',
+            'https://www.img.in.th/images/2f79add2e3c6d260e22496a3e2bcc4a2.jpg',
+            'https://www.img.in.th/images/ddd1aa09cafb58e704efcdb2f14c0e49.jpg'
+          ],
+          src: {
+            type: 'web',
+            link: ''
+          }
+        },
+        {
+          title: 'AIS D.C.',
+          desc: 'Application for AIS D.C.',
+          img: 'https://www.img.in.th/images/ba5a416747ffa3b75e0810cd11bc2a3a.jpg',
+          gallery: [
+            'https://www.img.in.th/images/ba5a416747ffa3b75e0810cd11bc2a3a.jpg',
+            'https://www.img.in.th/images/aec0d33defbfa8cfcda310dde470bae5.jpg',
+            'https://www.img.in.th/images/c1ef3272cb19116b713fc2523ac16ce1.jpg',
+            'https://www.img.in.th/images/ed78703f33cddff97467b5daa0039642.jpg'
+          ],
+          src: {
+            type: 'android',
+            link: 'https://play.google.com/store/apps/details?id=th.co.mimo.aistcdc&hl=th'
+          }
+        },
+        {
+          title: 'AIS FIBER ADMIN',
+          desc: 'Application Management Fiber Internet',
+          img: 'https://www.picz.in.th/images/2018/03/17/SxwSGl.jpg',
+          gallery: [
+            'https://www.picz.in.th/images/2018/03/17/SxwSGl.jpg',
+            'https://www.img.in.th/images/a50466ae0eeb1f5d499a7412ced355b8.jpg',
+            'https://www.img.in.th/images/8e41e5f5103a8794dafac9b0557f8726.jpg',
+            'https://www.img.in.th/images/b2eaf52f9b2ec731b6535bb01554c258.jpg'
+          ],
+          src: {
+            type: 'web',
+            link: ''
+          }
+        }
+      ],
+      skills: [
+        {variant: 'HTML5', value: 90},
+        {variant: 'Css', value: 90},
+        {variant: 'JavaScript', value: 80},
+        {variant: 'Android', value: 75},
+        {variant: 'PHP', value: 70},
+        {variant: 'Docker', value: 50}
+      ]
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
+    },
+    showModal (gallery) {
+      this.slidingImg = gallery
+      this.$refs.myModalRef.show()
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
   .bg {
     background-image: url("../assets/bg.jpg");
     background-repeat: no-repeat;
@@ -171,6 +238,13 @@ export default {
     left: 0;
     right: 0;
     margin-bottom: 10px;
+  }
+  .card {
+    cursor: pointer;
+  }
+
+  .carousel-control-next-icon, .carousel-control-prev-icon {
+    background-color: black;
   }
 
   .card:hover {
@@ -202,6 +276,10 @@ export default {
   .line {
     height: 5px;
     width: 100%;
+    background-color: rgb(237, 125, 49);
+  }
+
+  .progress-bar {
     background-color: rgb(237, 125, 49);
   }
 
@@ -244,6 +322,11 @@ export default {
 
   .bold {
     font-weight: bold;
+  }
+
+  .card-img-top{
+    height: 260px;
+    object-fit: cover;
   }
 
   @media screen and (max-width: 1200px) {
